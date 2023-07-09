0634475ec225033b404320f649c518aeb9f5fb49
knuxify <knuxify@gmail.com>
2023-07-09 14:28:14 +0200

    manually add images in photo posts to render
    fixes #2

--- fxtumblr/parser.py
+++ fxtumblr/parser.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from markdownify import markdownify
 
 
-async def get_trail(post: dict) -> dict:
+async def get_trail(post: dict, post_body: str = '') -> dict:
     trail = []
 
     # Custom handling for audio-only posts (type == 'audio'):
@@ -16,7 +16,7 @@ async def get_trail(post: dict) -> dict:
         info = {
             "type": "audio",
             "content": f"{post['track_name']} (1 audio file attached)",
-            "content_html": post['body'],
+            "content_html": f"{post['track_name']} (1 audio file attached)",
             "images": [post['album_art']]
         }
         if 'reblogged_root_name' in post:
@@ -32,7 +32,7 @@ async def get_trail(post: dict) -> dict:
         info = {
             "type": "video",
             "content": "(video)",
-            "content_html": post['body'],
+            "content_html": "(video)",
             "video": {
                 "url": post['video_url'],
                 "height": post['thumbnail_height'],
@@ -63,6 +63,10 @@ async def get_trail(post: dict) -> dict:
     # have to add it manually
     if post['type'] == 'photo':
         trail[0]['images'] = [photo['original_size']['url'] for photo in post['photos']]
+        images_include = ''
+        for image in trail[0]['images']:
+            images_include += f'<p><figure class="tmblr-full"><img src="{image}"></figure></p>'
+        trail[0]['content_html'] = images_include + trail[0]['content_html']
 
     if len(trail) == 1:
         if trail[0]['type'] == ['video']:
