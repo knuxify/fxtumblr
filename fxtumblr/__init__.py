45d7e024e3aa00b297317593aa0c262af0c5beea
knuxify <knuxify@gmail.com>
2023-07-09 12:11:06 +0200

    fix flake8 errors

--- fxtumblr/__init__.py
+++ fxtumblr/__init__.py
@@ -5,19 +5,18 @@ https://github.com/robinuniverse/TwitFix/
 
 (c) knuxify 2023, https://github.com/knuxify/fxtumblr
 """
-
 from quart import Quart, redirect
 from quart_cors import cors
 from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
-from .config import BASE_URL, config
+from .config import config
 
 # Initial setup to get things up and running
 app = Quart(__name__)  # Quart app
 app.asgi_app = ProxyHeadersMiddleware(app.asgi_app, trusted_hosts=["127.0.0.1"])
 cors(app)
 
-from . import embeds  # noqa: F401
+from . import embeds  # noqa: F401,E402
 if config['renders_enable']:
     from . import render  # noqa: F401
 
