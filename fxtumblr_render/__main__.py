06a65adc97783a3aeb0e9a25349ebbcda315b82e
knuxify <knuxify@gmail.com>
2024-06-14 19:11:44 +0200

    render: handle exceptions when closing

--- fxtumblr_render/__main__.py
+++ fxtumblr_render/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import traceback
 from .main import RenderServer
 
 server = RenderServer()
@@ -7,6 +8,9 @@ with asyncio.Runner() as runner:
     try:
         runner.run(server.main_loop())
     except (KeyboardInterrupt, RuntimeError, asyncio.CancelledError):
-        runner.run(server.on_exit())
+        try:
+            runner.run(server.on_exit())
+        except:
+            traceback.print_exc()
     finally:
         runner.close()
