b60f53ea0e52aba1ef13afdd8985dcf457872900
knuxify <knuxify@gmail.com>
2026-02-10 20:13:13 +0100

    tests: embed: fix embed test expected results

--- tests/embed/test_embed_meta.py
+++ tests/embed/test_embed_meta.py
@@ -12,7 +12,7 @@ def test_embed_class():
     embed = MetaEmbed(title="test")
 
     assert embed.to_meta_tags() == Markup(
-        '<meta property="twitter:card" content="summary_large_image""/><meta property="og:title" content="test"/><meta property="twitter:title" content="test"/>'
+        '<meta property="twitter:card" content="summary_large_image"/><meta property="og:title" content="test"/><meta property="twitter:title" content="test"/>'
     )
     assert embed.to_oembed() == {"version": "1.0", "type": "link", "title": "test"}
     assert (
@@ -20,17 +20,17 @@ def test_embed_class():
         == "https://example.com/_api/oembed.json?type=link&title=test"
     )
     assert embed.to_meta_tags(oembed_link=True) == Markup(
-        '<meta property="twitter:card" content="summary_large_image""/><meta property="og:title" content="test"/><meta property="twitter:title" content="test"/><link rel="alternate" type="application/json+oembed" href="https://example.com/_api/oembed.json?type=link&amp;title=test"/>'
+        '<meta property="twitter:card" content="summary_large_image"/><meta property="og:title" content="test"/><meta property="twitter:title" content="test"/><link rel="alternate" type="application/json+oembed" href="https://example.com/_api/oembed.json?type=link&amp;title=test"/>'
     )
 
     embed = MetaEmbed(title='"test?')
     assert embed.to_meta_tags() == Markup(
-        '<meta property="twitter:card" content="summary_large_image""/><meta property="og:title" content="&#34;test?"/><meta property="twitter:title" content="&#34;test?"/>'
+        '<meta property="twitter:card" content="summary_large_image"/><meta property="og:title" content="&#34;test?"/><meta property="twitter:title" content="&#34;test?"/>'
     )
     assert (
         embed.to_oembed_url()
         == "https://example.com/_api/oembed.json?type=link&title=%22test%3F"
     )
     assert embed.to_meta_tags(oembed_link=True) == Markup(
-        '<meta property="twitter:card" content="summary_large_image""/><meta property="og:title" content="&#34;test?"/><meta property="twitter:title" content="&#34;test?"/><link rel="alternate" type="application/json+oembed" href="https://example.com/_api/oembed.json?type=link&amp;title=%22test%3F"/>'
+        '<meta property="twitter:card" content="summary_large_image"/><meta property="og:title" content="&#34;test?"/><meta property="twitter:title" content="&#34;test?"/><link rel="alternate" type="application/json+oembed" href="https://example.com/_api/oembed.json?type=link&amp;title=%22test%3F"/>'
     )
