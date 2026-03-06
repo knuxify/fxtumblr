b43295a0e581e8123eea9bb624d7b5cf88b8be0c
knuxify <knuxify@gmail.com>
2026-03-06 16:07:55 +0100

    render: fix render error message not being formatted

--- fxtumblr_render/render.py
+++ fxtumblr_render/render.py
@@ -183,7 +183,7 @@ class RenderTaskPost(RenderTask):
         except Exception as e:
             if config.stats.enabled:
                 logger.error(
-                    "Error for {self.blog_name}-{self.post_id} ({self.modifiers}; {self.filetype}):"
+                    f"Error for {self.blog_name}-{self.post_id} ({self.modifiers}; {self.filetype}):"
                 )
                 await stats.increment_counter("render_error_count")
 
