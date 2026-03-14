78afd9646582460c28f2c719b293ee2eb28316d8
knuxify <knuxify@gmail.com>
2026-03-14 10:30:27 +0100

    tests: clean up methods of defining test API routes

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,157 +30,158 @@ def _get_tumblr_test_data(filename: str) -> str:
 def tumblr_api_server(httpserver: HTTPServer):
     """Test fixture that simulates Tumblr's API."""
 
-    # Test post 1: Ultimate NPF Test Post
-    # https://www.tumblr.com/knuxify/730903802869317632/the-ultimate-test-post-v2
-    with open(_get_tumblr_test_data("post_npftest.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/knuxify/posts",
+    # Helper functions
+
+    def _define_api_route(
+        data_filename: str,
+        path: str,
+        query_string: dict[str, str] | None = None,
+        status_code: int = 200,
+        is_json: bool = True,
+    ):
+        """Define API route in HTTP mock server."""
+        nonlocal httpserver
+        with open(_get_tumblr_test_data(data_filename)) as test_data:
+            request = httpserver.expect_request(path, query_string=query_string)
+            if is_json:
+                request.respond_with_json(json.load(test_data), status=status_code)
+            else:
+                request.respond_with_data(test_data.read(), status=status_code)
+
+    def _define_post(
+        data_filename: str,
+        blog_name: str,
+        post_id: int,
+        status_code: int = 200,
+        is_json: bool = True,
+    ):
+        """Define a post request."""
+        _define_api_route(
+            data_filename,
+            f"/v2/blog/{blog_name}/posts",
             query_string={
-                "id": "730903802869317632",
+                "id": str(post_id),
                 "npf": "true",
                 "api_key": "consumer_key",
             },
-        ).respond_with_json(json.load(test_data))
+            status_code=status_code,
+            is_json=is_json,
+        )
+
+    def _define_blog(
+        data_filename: str, blog_name: str, status_code: int = 200, is_json: bool = True
+    ):
+        """Define a blog request."""
+        nonlocal httpserver
+
+        _define_api_route(
+            data_filename,
+            f"/v2/blog/{blog_name}/info",
+            query_string={
+                "api_key": "consumer_key",
+            },
+            status_code=status_code,
+            is_json=is_json,
+        )
+
+    def _define_poll_results(
+        data_filename: str,
+        blog_name: str,
+        post_id: int,
+        poll_id: str,
+        status_code: int = 200,
+        is_json: bool = True,
+    ):
+        """Define a poll results request."""
+        nonlocal httpserver
+
+        _define_api_route(
+            data_filename,
+            f"/v2/polls/{blog_name}/{post_id}/{poll_id}/results",
+            query_string={
+                "api_key": "consumer_key",
+            },
+            status_code=status_code,
+            is_json=is_json,
+        )
+
+    # Test post 1: Ultimate NPF Test Post
+    # https://www.tumblr.com/knuxify/730903802869317632/the-ultimate-test-post-v2
+    _define_post("post_npftest.json", "knuxify", 730903802869317632)
 
     # Test post 2: Random reblog chain
     # https://www.tumblr.com/punkitt-is-here/781681205274886144
-    with open(_get_tumblr_test_data("post_reblogs.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/punkitt-is-here/posts",
-            query_string={
-                "id": "781681205274886144",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post("post_reblogs.json", "punkitt-is-here", 781681205274886144)
 
     # Test post 3: Answer to ask
-    with open(_get_tumblr_test_data("post_ask.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/knuxify/posts",
-            query_string={
-                "id": "732094755733929984",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post("post_ask.json", "knuxify", 732094755733929984)
 
     # Test post 4: Self-reblog, tags only
-    with open(_get_tumblr_test_data("post_self_reblog_tags_only.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/knuxify/posts",
-            query_string={
-                "id": "799921945450840064",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post("post_self_reblog_tags_only.json", "knuxify", 799921945450840064)
 
     # Special case post 1: Poll with missing answer
-    with open(_get_tumblr_test_data("post_broken_poll_answer.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/janmisali/posts",
-            query_string={
-                "id": "728090722324119552",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post("post_broken_poll_answer.json", "janmisali", 728090722324119552)
 
     # Special case post 2: Blocks in rows out of order, plus truncate_after
-    with open(
-        _get_tumblr_test_data("post_blocks_out_of_order_and_truncation.json")
-    ) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/toastyyjams/posts",
-            query_string={
-                "id": "808575515119255552",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post(
+        "post_blocks_out_of_order_and_truncation.json",
+        "toastyyjams",
+        808575515119255552,
+    )
 
     # Special case post 3: Image with its attribute value set to an empty list
-    with open(_get_tumblr_test_data("post_attrib_empty_list.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/mousegirlheart/posts",
-            query_string={
-                "id": "796276113056923648",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post("post_attrib_empty_list.json", "mousegirlheart", 796276113056923648)
 
     # Special case post 4: Posts listed as "hidden due to its potentially sensitive nature"
     # return a literal 404 HTML page through the API, due to what I can only assume
     # is a Tumblr bug?
-    with open(_get_tumblr_test_data("html_404.html")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/princessdollknight/posts",
-            query_string={
-                "id": "810480162328215552",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_data(test_data.read(), status=404)
+    _define_post(
+        "html_404.html",
+        "princessdollknight",
+        810480162328215552,
+        status_code=404,
+        is_json=False,
+    )
 
     # Special case post 5: Old post where part of the reblog chain was converted
     # into blockquotes
-    with open(_get_tumblr_test_data("post_reblog_chain_blockquotes.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/worldheritagepostorganization/posts",
-            query_string={
-                "id": "806582021235277824",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post(
+        "post_reblog_chain_blockquotes.json",
+        "worldheritagepostorganization",
+        806582021235277824,
+    )
 
     # Result of post_id=0
-    with open(_get_tumblr_test_data("post_id0.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/knuxify/posts",
-            query_string={"id": "0", "npf": "true", "api_key": "consumer_key"},
-        ).respond_with_json(json.load(test_data))
-
-    # Poll results for Ultimate NPF Test Post
-    with open(_get_tumblr_test_data("poll_results_npftest.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/polls/knuxify/730903802869317632/e040d07a-ca6a-4751-8df5-ebaa1719222e/results",
-            query_string={
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_post("post_id0.json", "knuxify", 0)
 
     # Test blog 1: knuxify
     # https://www.tumblr.com/knuxify
-    with open(_get_tumblr_test_data("blog_knuxify.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/knuxify/info",
-            query_string={
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_blog("blog_knuxify.json", "knuxify", status_code=404, is_json=False)
+
+    # Poll results for Ultimate NPF Test Post
+    _define_poll_results(
+        "poll_results_npftest.json",
+        "knuxify",
+        730903802869317632,
+        "e040d07a-ca6a-4751-8df5-ebaa1719222e",
+    )
 
     # Poll results: invalid poll ID
-    with open(_get_tumblr_test_data("poll_results_invalid_id.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/polls/knuxify/730903802869317632/bogus/results",
-            query_string={
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data), status=400)
+    _define_poll_results(
+        "poll_results_invalid_id.json",
+        "knuxify",
+        730903802869317632,
+        "bogus",
+        status_code=400,
+    )
 
     # Poll results for broken poll answer test
-    with open(
-        _get_tumblr_test_data("poll_results_broken_poll_answer.json")
-    ) as test_data:
-        httpserver.expect_request(
-            "/v2/polls/janmisali/728090722324119552/d0b6b130-0f4b-46b1-af77-1ea386c85529/results",
-            query_string={
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data))
+    _define_poll_results(
+        "poll_results_broken_poll_answer.json",
+        "janmisali",
+        728090722324119552,
+        "d0b6b130-0f4b-46b1-af77-1ea386c85529",
+    )
 
     # 404 responses
     with open(_get_tumblr_test_data("resp_404.json")) as test_data:
@@ -215,22 +216,12 @@ def tumblr_api_server(httpserver: HTTPServer):
         ).respond_with_json(data, status=404)
 
     # Private blog
-    with open(_get_tumblr_test_data("blog_private.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/private-blog-test/info",
-            query_string={"api_key": "consumer_key"},
-        ).respond_with_json(json.load(test_data), status=404)
+    _define_blog("blog_private.json", "private-blog-test", status_code=404)
 
     # Private post
-    with open(_get_tumblr_test_data("post_private.json")) as test_data:
-        httpserver.expect_request(
-            "/v2/blog/private-blog-test/posts",
-            query_string={
-                "id": "808188296199094272",
-                "npf": "true",
-                "api_key": "consumer_key",
-            },
-        ).respond_with_json(json.load(test_data), status=404)
+    _define_post(
+        "post_private.json", "private-blog-test", 808188296199094272, status_code=404
+    )
 
     return httpserver
 
